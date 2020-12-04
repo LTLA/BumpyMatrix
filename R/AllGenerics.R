48e3c62c3a8599d7cf46406f10314fc68d1db322
Aaron Lun <lun.aaron@gene.com>
2020-12-03 16:12:23 -0800

    Removed redundant 'field' entries.

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,8 +1,2 @@
 #' @export 
 setGeneric("undim", function(x) standardGeneric("undim"))
-
-#' @export 
-setGeneric("fields", function(x) standardGeneric("fields"))
-
-#' @export 
-setGeneric("fields<-", function(x, value) standardGeneric("fields<-"))
